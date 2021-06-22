9f4bfe7462169aee1ee5d30ef5d2d1f8831dd68e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-06-23 00:54:43 +0900

    Update 3D-Surface-Area.cpp

--- 3D-Surface-Area.cpp
+++ 3D-Surface-Area.cpp
@@ -1,1 +1,20 @@
-
+int surfaceArea(vector<vector<int>> A)
+{
+    int sum = 0;
+    
+    for (int i = 0; i < A.size(); ++i)
+    {
+        for (int j = 0; j < A[0].size(); ++j)
+        {
+            sum += A[i][j] * 4 + 2;
+            
+            if (j+1 < A[0].size())
+                sum -= 2 * min(A[i][j], A[i][j+1]);
+            
+            if (i+1 < A.size())
+                sum -= 2 * min(A[i][j], A[i+1][j]);
+        }
+    }
+    
+    return sum;
+}    
