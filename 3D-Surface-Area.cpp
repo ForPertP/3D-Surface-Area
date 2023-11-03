d42910a65ddfe4a34aacb5613dd1cd5e2354f4ce
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-11-04 00:39:29 +0900

    Update 3D-Surface-Area.cpp

--- 3D-Surface-Area.cpp
+++ 3D-Surface-Area.cpp
@@ -33,3 +33,42 @@ int surfaceArea(vector<vector<int>> A)
     
     return sum;
 }    
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int H = stoi(first_multiple_input[0]);
+
+    int W = stoi(first_multiple_input[1]);
+
+    vector<vector<int>> A(H);
+
+    for (int i = 0; i < H; i++) {
+        A[i].resize(W);
+
+        string A_row_temp_temp;
+        getline(cin, A_row_temp_temp);
+
+        vector<string> A_row_temp = split(rtrim(A_row_temp_temp));
+
+        for (int j = 0; j < W; j++) {
+            int A_row_item = stoi(A_row_temp[j]);
+
+            A[i][j] = A_row_item;
+        }
+    }
+
+    int result = surfaceArea(A);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
