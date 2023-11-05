c1866b9e09ea7d33e73c415d27a82174cdf77765
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-11-06 03:54:11 +0900

    Create 3D-Surface-Area.cs

---
+++ 3D-Surface-Area.cs
@@ -0,0 +1,24 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'surfaceArea' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY A as parameter.
+     */
+}
