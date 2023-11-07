7f80d6b5bd9898eeebe7783fe05034cac7eb467b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-11-08 01:38:47 +0900

    Update 3D-Surface-Area.cs

--- 3D-Surface-Area.cs
+++ 3D-Surface-Area.cs
@@ -21,8 +21,29 @@ class Result
      * The function is expected to return an INTEGER.
      * The function accepts 2D_INTEGER_ARRAY A as parameter.
      */
-}
 
+    public static int surfaceArea(List<List<int>> A)
+    {
+       int sum = 0;
+
+        for (int i = 0; i < A.Count; ++i)
+        {
+            for (int j = 0; j < A[0].Count; ++j)
+            {
+                sum += A[i][j] * 4 + 2;
+
+                if (j + 1 < A[0].Count)
+                    sum -= 2 * Math.Min(A[i][j], A[i][j + 1]);
+
+                if (i + 1 < A.Count)
+                    sum -= 2 * Math.Min(A[i][j], A[i + 1][j]);
+            }
+        }
+
+        return sum;
+    }
+
+}
 
 class Solution
 {
